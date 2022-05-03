fdc3dbfe69a8212c5d72fcb91767ad7f75249869
lucas dalan <75147965+lucasd09@users.noreply.github.com>
2022-05-03 09:04:42 -0300

    about

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
+import photo from '../../utils/photo.png';
 
-import { AboutCard, Container, Title } from './styles';
+import { AboutCard, CardContent, Container, Photo, Title, Wrapper, Text, TitleCard } from './styles';
 
 const About: React.FC = () => {
   return <Container>
-    <Title>About</Title>
-    <AboutCard>
-      
-    </AboutCard>
+    <Wrapper>
+      <TitleCard>
+        <Title fontSize='64px'>About</Title>
+      </TitleCard>
+      <AboutCard>
+        <Photo src={photo}/>
+        <CardContent>
+          <Title fontSize='48px'>Lucas Dalan</Title>
+          <Text fontSize='24px'>Software Developer</Text>
+          <Text fontSize='24px'>React.js | UI/UX | Python | Flutter</Text>
+        </CardContent>
+      </AboutCard>
+      <Text fontSize='24px'>Hi! my name is Lucas, I’m 20 years old and I work as a Developer.
+My main goal is to have my own IT company, but in order to acomplish
+that I must start from the beginning. when I work on  important, 
+modern projects I get excited to develop it, making me even more 
+eager to see it working perfectly.</Text>
+    </Wrapper>
   </Container>;
 }
 
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,22 +1,55 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  height: 100vh;
   width: 100%;
-  border: 1px solid green;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const Title = styled.h1`
-  font-size: 64px;
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1280px;
+
+`;
+
+export const Title = styled.h1<{
+  fontSize?: string;
+}>`
+  font-size: ${props => props.fontSize || 'auto'};
   color: var(--primary);
 `;
 
+export const TitleCard = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: start;
+`;
+
 export const AboutCard = styled.div`
-  max-width: 1280px;
+  display: flex;
+  color: var(--primary);
+  margin-bottom: 67px;
+`;
+
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-left: 25px;
+`;
+
+export const Text = styled.p<{
+  fontSize?: string;
+}>`
+  font-size: ${props => props.fontSize || 'auto'};
   color: var(--primary);
 `;
 
 export const Photo = styled.img`
   width: 252px;
   height: 253px;
-  
 `;
